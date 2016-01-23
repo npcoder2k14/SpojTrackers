6d24ca8078e0e254cba04b2c0460b21d28879c8c
Narendra Pal <npnarendrapal23@gmail.com>
2016-01-24 03:46:00 +0530

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,11 +1,11 @@
 <?php
 ob_start("ob_gzhandler");
 include('simple_html_dom.php');
-$url = 'http://www.whatsmyip.org/http_compression/';
+$url = 'http://www.spoj.com/users/npcoder2k14';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url); // Define target site
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Return page in string
-curl_setopt($cr, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.2 (KHTML, like Gecko) Chrome/5.0.342.3 Safari/533.2');
+curl_setopt($cr, CURLOPT_USERAGENT, HTTP_USER_AGENT);
 curl_setopt($ch, CURLOPT_ENCODING , "gzip");     
 curl_setopt($ch, CURLOPT_TIMEOUT,5); 
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); // Follow redirects
