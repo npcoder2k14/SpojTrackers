9ec2faf454c8ff619d6073f1a89231641047f003
npcoder2k14 <npnarendrapal23@gmail.com>
2016-01-24 01:02:28 +0530

    added curl with html_dom

--- proxy.php
+++ proxy.php
@@ -1,10 +1,13 @@
 <?php
-$auth=base64_encode('edcguest:edcguest');
+/*$auth=base64_encode('edcguest:edcguest');
     			$Context=array('http'=>array(
-	        			'proxy'=>'tcp://172.31.103.29:3128',
+	        			'proxy'=>'tcp://172.31.102.14:3128',
 	        			'request_fulluri'=>true,
 	        			'header'=>"Proxy-Authorization:Basic $auth"
    )
 );
-$cxt=stream_context_create($Context);
+$cxt=stream_context_create($Context);   */
+      $proxy='172.31.102.29:3128';
+      $proxyauth='edcguest:edcguest';
+
 ?>
