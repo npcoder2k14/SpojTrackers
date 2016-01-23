893d064e8176527b010ded8e635e5e3b31165b83
npcoder2k14 <npnarendrapal23@gmail.com>
2016-01-24 02:00:46 +0530

    testing

--- 1.php
+++ 1.php
@@ -1,6 +1,6 @@
 <?php
 include('simple_html_dom.php');
-include('proxy.php');
+//include('proxy.php');
 $url1="http://www.spoj.com/users/npcoder2k14/";
 //  $html1 = file_get_html($url1,false,$cxt);
 echo $url1;
@@ -8,8 +8,8 @@ $ch=curl_init();
 curl_setopt($ch, CURLOPT_URL,$url1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($ch,CURLOPT_PROXY,$proxy);
-curl_setopt($ch,CURLOPT_PROXYUSERPWD,$proxyauth);
+//curl_setopt($ch,CURLOPT_PROXY,$proxy);
+//curl_setopt($ch,CURLOPT_PROXYUSERPWD,$proxyauth);
 $str = curl_exec($ch);
 curl_close($ch);
 echo  "hi fuck";
